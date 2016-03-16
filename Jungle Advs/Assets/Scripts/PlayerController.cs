79b4018723283f375e48616d1ef7436d5fd323c2
buitu3 <buitu3@gmail.com>
2016-03-16 10:59:53 +0700

    Upload project

---
+++ Jungle Advs/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerWalkSpd;
+    public float jumpForce;
+    public float doubleJumpForce;
+
+    private Transform playerTransform;
+    private Rigidbody2D playerRigidbody;
+
+    [HideInInspector]
+    public bool isGrounded = true;
+    [HideInInspector]
+    public bool isJumping = false;
+    [HideInInspector]
+    public bool isDoubleJumping = false;
+    private bool isFacingLeft = false;
+    [HideInInspector]
+    public bool isFalling = false;
+
+    //public enum playerStates
+    //{
+    //    idle = 0,
+    //    left,
+    //    right,
+    //    jump,
+    //    doublejump,
+    //    falling,
+    //    death
+    //}
+    Vector2 movement;
+	
+	void Start () {
+        playerTransform = GetComponent<Transform>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h != 0)
+        {
+            Move(h);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                Jump();
+                print("jumped");
+            }
+            else if (isJumping && !isFalling)
+            {
+                doubleJump();
+                print("doublejumped");
+            }
+        }
+	}
+
+    void Move(float h)
+    {
+        if ((h > 0 && isFacingLeft) || (h < 0 && !isFacingLeft))
+        {
+            Flip();
+        }
+
+        movement.Set(playerWalkSpd * h * Time.deltaTime, 0f);
+        //playerTransform.Translate(movement);
+        playerRigidbody.velocity = new Vector2(playerWalkSpd * h * Time.deltaTime, playerRigidbody.velocity.y);
+    }
+
+    void Jump()
+    {
+        playerRigidbody.AddForce(new Vector2(0f, jumpForce));
+        isGrounded = false;
+        isJumping = true;
+    }      
+
+    void doubleJump()
+    {
+        playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
+        playerRigidbody.AddForce(new Vector2(0f, doubleJumpForce));
+        isJumping = false;
+        isDoubleJumping = true;
+    }
+
+    private void Flip()
+    {
+        isFacingLeft = !isFacingLeft;
+        playerTransform.localScale = new Vector3(playerTransform.localScale.x * -1, playerTransform.localScale.y, playerTransform.localScale.z);
+    }
+}
---
+++ Jungle Advs/Assets/Scripts/PlayerGroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGroundCheck : MonoBehaviour {
+
+    public PlayerController playerControl;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Platform")
+        {
+            playerControl.isJumping = false;
+            playerControl.isDoubleJumping = false;
+            playerControl.isFalling = false;
+            playerControl.isGrounded = true;
+            
+            print("grounded");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Platform" && !playerControl.isJumping)
+        {
+            playerControl.isGrounded = false;
+            playerControl.isFalling = true;
+        }
+    }
+}
